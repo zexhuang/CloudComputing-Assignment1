a58fbc38cac6036781e3c196aa950dd33407877c
zexhuang <k305261558@icloud.com>
2019-03-24 17:26:01 +1100

    update: readdata function

--- assignment1.py
+++ assignment1.py
@@ -3,19 +3,32 @@
 import json
 from mpi4py import MPI
 import time
-     
-beginninga_time = time.time()
-# read grids file
-grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
-with open(grids_file_path) as json_file:  
-    grids_data = json.load(json_file)
-# read twitter file
-twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
-with open(twitter_file_path) as json_file:  
-    twitter_data = json.load(json_file)
 
-grid = dict()
-for feature in mg['features']:
-    grid.update({feature['properties']['id']: feature['geometry']['coordinates'][0]})
+def processGrids():
+        # read grids file
+    grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
+    with open(grids_file_path) as json_file:  
+        grids_data = json.load(json_file)
+        coordinates = grids_data['features']
+    return dict()
 
-end_time = time.time()
+def processTwitters():
+        # read twitter file
+    twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
+    with open(twitter_file_path) as json_file:  
+        twitter_data = json.load(json_file)
+    return dict()
+
+def main():
+    beginninga_time = time.time()
+
+    myGrids = processGrids()
+    myTwitter = processTwitters()
+
+    end_time = time.time()
+
+    used_time = end_time - beginninga_time
+    print (used_time)
+
+if __name__== "__main__":
+  main()
