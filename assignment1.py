07b574a1caa500a60e361c7d74966dee7681c6c0
gdzqzzx <zhengzhixin2016@gmail.com>
2019-03-24 18:19:50 +1100

    updated processGrids(), read in line

--- assignment1.py
+++ assignment1.py
@@ -1,33 +1,41 @@
 # Authors: Zexian Huang 
 # Date: March 24 2019
 import json
-from mpi4py import MPI
+#from mpi4py import MPI
 import time
 
-def processGrids():
-    # read grids file
-    grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
-    with open(grids_file_path) as json_file:  
+def processGrids(fpath):
+        # read grids file
+    grids_features = {}
+    with open(fpath) as json_file:
         grids_data = json.load(json_file)
-        coordinates = grids_data['features']
-    return dict()
+        for feature in grids_data['features']:
+            grids_features.update({feature['properties']['id']: feature['geometry']['coordinates'][0]})
+    return grids_features
 
-def processTwitters():
-    # read twitter file
-    twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
-    with open(twitter_file_path) as json_file:  
+def processTwitters(fpath):
+        # read twitter file
+    with open(fpath) as json_file:
         twitter_data = json.load(json_file)
-    return dict()
+    return twitter_data
 
 def main():
     beginninga_time = time.time()
 
-    myGrids = processGrids()
-    myTwitter = processTwitters()
+    #grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
+    grids_file_path = r"D:\Download\CCC\melbGrid.json"
+    #twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
+    twitter_file_path = r'D:\Download\CCC\tinyTwitter.json'
+
+    myGrids = processGrids(grids_file_path)
+    myTwitter = processTwitters(twitter_file_path)
+    myGrids
 
     end_time = time.time()
+
     used_time = end_time - beginninga_time
     print (used_time)
 
 if __name__== "__main__":
     main()
+
