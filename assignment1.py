2806a5a9d0da7f9a1f73e57602fd906d6f637df7
zexhuang <k305261558@icloud.com>
2019-03-24 17:15:15 +1100

    update: filter grids data

--- assignment1.py
+++ assignment1.py
@@ -1,18 +1,32 @@
 # Authors: Zexian Huang 
 # Date: March 24 2019
+
 import json
 from mpi4py import MPI
 import time
-     
-beginninga_time = time.time()
-# read grids file
-grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
-with open(grids_file_path) as json_file:  
-    grids_data = json.load(json_file)
-# read twitter file
-twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
-with open(twitter_file_path) as json_file:  
-    twitter_data = json.load(json_file)
 
+def clean_twitter_data(): 
+    print('data now is clean!')
 
-end_time = time.time()
+def main():
+    beginninga_time = time.time()
+    num_of_grids = 16
+
+    # read grids file
+    grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
+    with open(grids_file_path) as json_file:  
+        grids_data = json.load(json_file)
+        grids_coordinates = grids_data ['features'] 
+        
+    # read twitter file
+    twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/tinyTwitter.json'
+    with open(twitter_file_path) as json_file:  
+        twitter_data = json.load(json_file)
+
+    end_time = time.time()
+
+    used_time = end_time - beginninga_time
+    print (used_time)
+
+if __name__== "__main__":
+  main()
