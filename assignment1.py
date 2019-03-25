3daa59fe1b51be8815b0dd33d14a0492fe3bc44c
zexhuang <k305261558@icloud.com>
2019-03-25 16:04:53 +1100

    add: new grids function

--- assignment1.py
+++ assignment1.py
@@ -3,9 +3,11 @@
 import json
 from mpi4py import MPI
 import time
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 def processGrids(fpath):
-        # read grids file
+    # read grids file
     # grids_features is a dictionary of tuples, storing grid_id as keys and coordinates-polygon pairs as tuple
     grids_features = {}
     with open(fpath, encoding = 'UTF-8') as json_file:
@@ -20,12 +22,30 @@ def processTwitters(fpath):
         twitter_data = json.load(json_file)
     return twitter_data
 
+# This function is for returning a list of large grids, such as grid A, B, C, D 
+def largeGridsList(xmin, ymin, xmax, ymax):
+    return list()
+
+# This function is for returning a list of small areas, such as grid A1, A2, A3, A4
+def smallGridsList(c1,c2,c3,c4,c5):
+    return list()
+
+def checkPointInPolygon (x,y):
+    point = Point(x,y)
+    print(point)
+
+    # point = Point(0.5, 0.5)
+    # polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+    # print(polygon.contains(point)) -> boolean value
+
+    return bool
+
 def main():
     beginninga_time = time.time()
 
-    #grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
+    # grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
     grids_file_path = r"D:\Download\CCC\melbGrid.json"
-    #twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
+    # twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
     twitter_file_path = r'D:\Download\CCC\tinyTwitter.json'
 
     myGrids = processGrids(grids_file_path)
