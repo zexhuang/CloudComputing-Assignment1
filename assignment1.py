bc13903fdefdc028dd38d926f1136a1dd6fa9211
gdzqzzx <zhengzhixin2016@gmail.com>
2019-03-26 16:59:26 +1100

    Revert "Debug processTwitters and change list of coordinate into tuple of coordinate in proccessGrids"
    This reverts commit 056172b6024f3d9070a1aeabbed8c0e823bf440c.

--- assignment1.py
+++ assignment1.py
@@ -23,7 +23,7 @@ def processGrids(fpath):
             # 0'xmin', 1'ymin', 2'xmax', 3'ymax'
             ploygon = tuple(pd.concat([ploygons.filter(like = name).loc[[0, 2], :].min(axis = 1), ploygons.filter(like = name).loc[[1, 3], :].max(axis = 1)]))
             coordinate = coordinates.filter(like = name).to_dict()
-            coordinate.update(map(lambda x: (x, tuple(coordinate[x].values())), coordinate.keys()))
+            coordinate.update(map(lambda x: (x, list(coordinate[x].values())), coordinate.keys()))
             grids_features.update({ploygon: coordinate})
     return grids_features
 
@@ -33,39 +33,47 @@ def processTwitters(fpath):
     with open(fpath, encoding = 'UTF-8') as json_file:
         twitter_data = json.load(json_file)
         for row in twitter_data['rows']:
-            if row['doc']['entities']['hashtags'] and row['value']['geometry']['coordinates']:
-                twitter_features.update({tuple(row['value']['geometry']['coordinates']): tuple([row['doc']['entities']['hashtags'][0]['text']])})
+            if row['doc']['entities']['hashtags']:
+                twitter_features.update({tuple(row['value']['geometry']['coordinates']): row['doc']['entities']['hashtags'][0]['text']})
         # we still need to parse the json file in iteration
     return twitter_features
 
-# This function is for returning a list of large grids, such as grid A, B, C, D 
-def largeGridsList(xmin, ymin, xmax, ymax):
-    return list()
+# This function is for returning a dic of large grids, such as grid A, B, C, D 
+def largeGrids(grids_features:dict):
+    name = ['A', 'B', 'C', 'D']
+    largeGrids = []
+    for coord in grids_features.keys():
+        largeGrids.append(coord)
+    
+    return dict(zip(name,largeGrids))
 
-# This function is for returning a list of small areas, such as grid A1, A2, A3, A4
-def smallGridsList(c1,c2,c3,c4,c5):
-    return list()
+def smallGrids(grids_features:dict):
+    smallGrids = []
+    for area in grids_features.values():
+        smallGrids.append(area)
+
+    return smallGrids
 
 def checkPointInPolygon (x,y):
     point = Point(x,y)
-    print(point)
-
     # point = Point(0.5, 0.5)
     # polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
     # print(polygon.contains(point)) -> boolean value
-
     return bool
 
 def main():
     beginninga_time = time.time()
 
-    # grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
-    grids_file_path = r"D:\Download\CCC\melbGrid.json"
-    # twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
-    twitter_file_path = r'D:\Download\CCC\tinyTwitter.json'
+    grids_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/melbGrid.json'
+    # grids_file_path = r"D:\Download\CCC\melbGrid.json"
+    twitter_file_path = '/Users/Huangzexian/Downloads/CloudComputing/assignment1-remote/smallTwitter.json'
+    # twitter_file_path = r'D:\Download\CCC\tinyTwitter.json'
 
     myGrids = processGrids(grids_file_path)
     myTwitter = processTwitters(twitter_file_path)
+
+    largeGrids(myGrids)
+    smallGrids(myGrids)
 
     end_time = time.time()
 
