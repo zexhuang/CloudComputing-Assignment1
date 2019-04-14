a6106806935024fd8a4e2831bec13a59c8d3c8f7
zexhuang <k305261558@icloud.com>
2019-04-14 21:00:00 +1000

    update: new bar chart code

--- resultComparison.py
+++ resultComparison.py
@@ -2,13 +2,32 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
  
-objects = ('1 node and 1 core', '1 node and 8 cores', '2 nodes and 8 cores')
-y_pos = np.arange(len(objects))
-performance = [64.965988, 47.490299,47.507174]
+# objects = ('1 node and 1 core', '1 node and 8 cores', '2 nodes and 8 cores')
+# y_pos = np.arange(len(objects))
+# performance = [68.285044, 45.840233, 46.463449]
+# label = [68.28, 45.84, 46.46]
  
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Time (Sceonds)')
-plt.title('Performance')
+# plt.bar(y_pos, performance, align='center', alpha=0.5)
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Time (Sceonds)')
+# plt.title('Performance')
+
+# for i in range(len(objects)):
+#     plt.text(x = i - 0.1, y = label[i] + 1, s = str(label[i]) + "s", size = 8)
  
-plt.show()
+# plt.show()
+
+# area = ('C2', 'B2', 'C3', 'B3', 'C4', 'B1','D4', 'D3', 'C1', 'B4', 'A3', 'A2', 'C5','D5','A1','A4')
+# y_pos = np.arange(len(area))
+# post_count = [145136, 79639, 53308, 26554, 19700, 16508, 14497, 13389, 8427, 5450, 5036, 4165, 4105, 3249, 2533, 310]
+# label = [145136, 79639, 53308, 26554, 19700, 16508, 14497, 13389, 8427, 5450, 5036, 4165, 4105, 3249, 2533, 310]
+ 
+# plt.bar(y_pos, post_count, align='center', alpha=0.5)
+# plt.xticks(y_pos, area)
+# plt.ylabel('Number of twitters')
+# plt.title('Post Count')
+
+# for i in range(len(area)):
+#     plt.text(x = i-0.4, y = label[i] + 1, s = str(label[i]), size = 6)
+ 
+# plt.show()
